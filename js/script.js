26a057f38d72f3c5d4ef80bb8b9fc7fd8abdff1d
comemaino <mat.maino@gmail.com>
2022-04-22 16:56:15 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Generare una griglia di gioco quadrata in cui ogni cella contiene un numero compreso tra 1 e 100.
+// Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro.Bonus:
+// Aggiungere la possibilità di scegliere un livello di difficoltà in base al quale viene generata una griglia di uno dei seguenti range:
+// con difficoltà 1 => tra 1 e 100
+// con difficoltà 2 => tra 1 e 81
+// con difficoltà 3 => tra 1 e 49
